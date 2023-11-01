5b5c1475eca83188e67f76778d319a3bf9f688e4
Prashanth Rao <35005448+prrao87@users.noreply.github.com>
2023-10-31 22:59:25 -0400

    Simplify pydantic schema

--- elasticsearch/schemas/wine.py
+++ elasticsearch/schemas/wine.py
@@ -51,23 +51,8 @@ class Wine(BaseModel):
         return values
 
 
-class FullTextSearchModel(BaseModel):
-    model_config = ConfigDict(
-        extra="ignore",
-        json_schema_extra={
-            "example": {
-                "wineID": 3845,
-                "country": "Italy",
-                "title": "Castellinuzza e Piuca 2010  Chianti Classico",
-                "description": "This gorgeous Chianti Classico boasts lively cherry, strawberry and violet aromas. The mouthwatering palate shows concentrated wild-cherry flavor layered with mint, white pepper and clove. It has fresh acidity and firm tannins that will develop complexity with more bottle age. A textbook Chianti Classico.",
-                "points": 93,
-                "price": 16,
-                "variety": "Red Blend",
-                "winery": "Castellinuzza e Piuca",
-            }
-        },
-    )
-
+class SearchResult(BaseModel):
+    "Model to return search results"
     id: int
     title: str
     description: Optional[str]
